021f22cbb02a14bff2ba7ca88431516c161ce0cb
Bennani <asbennani95@gmail.com>
2019-10-23 15:56:19 +0200

    init commit

---
+++ ticket-batch/src/main/java/com/upem/demo/batch/App.java
@@ -0,0 +1,13 @@
+package com.upem.demo.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ticket-consumer/src/main/java/com/upem/demo/consumer/App.java
@@ -0,0 +1,13 @@
+package com.upem.demo.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
